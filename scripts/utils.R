00e9141d0f8fe9ec6c08c2bd1a6987adc72c9e12
andrew <andrewazman@gmail.com>
2021-02-10 20:58:21 +0100

    fixing a few small issues from email

--- scripts/utils.R
+++ scripts/utils.R
@@ -19,7 +19,7 @@ reload_source <- function() {
   library("RColorBrewer")
   if (!require("knitr")) install.packages("knitr")
   library("knitr")
-  
+
   source("source/utils.R")
 }
 
@@ -40,12 +40,12 @@ reload_source <- function() {
 #' @param age_ref reference category for age
 #' @param wk_ref reference category for serosurvey week
 #' @param redo redo fit or load pre-computed posteriors if available
-#' 
+#'
 #' @details The analysis parameter needs to be one of:
 #' - ei: main analysis with manufacturer threshold
 #' - gva: custom Geneva threshold
-#' - ifa: alternative thershold
-#' 
+#' - ifa: alternative threshold
+#'
 #' @return a list with parameter posteriors and results
 run_analysis_stan_re <- function(model_script,
                                  dat,
@@ -64,7 +64,7 @@ run_analysis_stan_re <- function(model_script,
                                  chains,
                                  iter,
                                  warmup,
-                                 control, 
+                                 control,
                                  ...) {
   ## Prepare data for analysis
   ana_suffix <- case_when(analysis == "ei" ~ "",
@@ -73,28 +73,29 @@ run_analysis_stan_re <- function(model_script,
                           T ~ as.character(NA))
   if (is.na(ana_suffix))
     stop("Unkonwn analysis parameter, must be one of ei, gva, ifa")
-  
+
   # Set analysis data
   ana_dat <- as_tibble(dat)
-  
+
   for (var in c("pos", "neg", "ind")) {
     ana_dat[[var]] <- as.numeric(dat[[paste0(var, ana_suffix)]])
   }
-  
+
   # Set model matrix
   X <- model.matrix(as.formula(paste("~", coef_eqn)), data = ana_dat)
-  
-  # name of the stan output file 
+
+  # name of the stan output file
+  if (!dir.exists('results')) {dir.create('results')}
   stan_out_file <- paste0("results/stan_fit_", analysis,".rds")
-  
+
   if (!file.exists(stan_out_file) | redo) {
-    
+
     re_model <- stan_model(model_script)
-    
+
     # Unique household ids from 1 to N
     u_hh_ids <- unique(ana_dat$household_id)
     ana_dat$u_household_id <- map_dbl(ana_dat$household_id, ~which(u_hh_ids == .))
-    
+
     # Run stan model
     stan_est <- sampling(re_model,
                          data = list(
@@ -113,37 +114,37 @@ run_analysis_stan_re <- function(model_script,
                          iter = iter,
                          warmup = warmup,
                          control = control)
-    
+
     saveRDS(stan_est, stan_out_file)
   } else {
     cat("Loading pre-computed posteriors from ", stan_out_file, "\n")
     stan_est <- readRDS(stan_out_file)
   }
-  
+
   ## extract log-likelihoods
   stan_ll <- loo::extract_log_lik(stan_est) %>% loo::loo()
-  
+
   ## extract parameters
   beta <- extract(stan_est, pars = "beta")[[1]]
   sigma <- extract(stan_est, pars = "sigma_h")[[1]]
-  
+
   pop_cat_mat <- pop_age_cats %>%
     model.matrix(as.formula(paste("~", coef_eqn)), data = .)
-  
+
   ## compute estimates by age category
   cl <- parallel::makeCluster(n_cores)
   doParallel::registerDoParallel(cl)
-  
+
   pop_cat_p <- foreach(i = 1:nrow(pop_cat_mat),
-                       .combine = rbind, 
-                       .inorder = F, 
-                       .packages = c("tidyverse", "foreach")) %dopar% 
-    { 
-      foreach(j = 1:nrow(beta), 
-              .combine = rbind, 
-              .inorder = T) %do% 
+                       .combine = rbind,
+                       .inorder = F,
+                       .packages = c("tidyverse", "foreach")) %dopar%
+    {
+      foreach(j = 1:nrow(beta),
+              .combine = rbind,
+              .inorder = T) %do%
         {
-          
+
           # Compute probability integrating across in household random effects
           prob <- integrate(function(x) {
             plogis(qnorm(
@@ -151,7 +152,7 @@ run_analysis_stan_re <- function(model_script,
               sigma[j]
             ))
           }, 0, 1)[[1]]
-          
+
           tibble(
             age_cat = pop_age_cats$age_cat[i],
             Sex = pop_age_cats$Sex[i],
@@ -163,7 +164,7 @@ run_analysis_stan_re <- function(model_script,
         }
     }
   parallel::stopCluster(cl)
-  
+
   ## overall estimate
   overall_re <- pop_cat_p %>%
     filter(week != 1) %>%
@@ -174,7 +175,7 @@ run_analysis_stan_re <- function(model_script,
     group_by(sim, var, val) %>%
     summarize(p = weighted.mean(seropos, pop)) %>%
     ungroup()
-  
+
   # weekyl estimates
   wk_re <- pop_cat_p %>%
     mutate(var = "Week") %>%
@@ -182,7 +183,7 @@ run_analysis_stan_re <- function(model_script,
     group_by(sim, var, val) %>%
     summarize(p = weighted.mean(seropos, pop)) %>%
     ungroup()
-  
+
   ## find age specific probabilities in order to make relative risks
   age_re <- pop_cat_p %>%
     filter(Sex == sex_ref, week == wk_ref) %>%
@@ -191,7 +192,7 @@ run_analysis_stan_re <- function(model_script,
     group_by(sim, var, val) %>%
     summarize(p = weighted.mean(seropos, pop)) %>%
     ungroup()
-  
+
   # sex-specific probabilities
   sex_re <- pop_cat_p %>%
     filter(age_cat == age_ref, week == wk_ref) %>%
@@ -200,7 +201,7 @@ run_analysis_stan_re <- function(model_script,
     group_by(sim, var, val) %>%
     summarize(p = weighted.mean(seropos, pop)) %>%
     ungroup()
-  
+
   # restuls list
   res <- list(
     beta = extract(stan_est, pars = "beta")[[1]],
@@ -221,6 +222,6 @@ run_analysis_stan_re <- function(model_script,
       age_re
     )
   )
-  
+
   return(res)
 }
